75d912230be72f680de790911816ef794c0c9676
Khalil Aliev <khalilaliev@Khalil.local>
2024-06-08 14:56:36 +0200

    fixed hw-5

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,40 @@
-interface IContentsOfFigures {
+abstract class Figure {
   readonly name: string;
   readonly color: string;
-  calculateArea: (side1: number, side2: number) => number;
-}
-
-class Circle implements IContentsOfFigures {
-  readonly name: string;
-  readonly color: string;
-  public radius: number;
-  constructor(name: string, color: string, radius: number) {
+  constructor(name: string, color: string) {
     this.name = name;
     this.color = color;
+  }
+
+  abstract calculateArea(): number;
+  print?(): string;
+}
+
+// ---------------------------------------------------
+class Circle extends Figure {
+  public radius: number;
+
+  constructor(color: string, radius: number) {
+    super("Circle", color);
     this.radius = radius;
   }
 
   calculateArea(): number {
-    const area = Math.PI * Math.pow(this.radius, 2);
+    const area = Math.PI * this.radius * this.radius;
     return area;
   }
 }
 
-const circle = new Circle("Circle", "Red", 12);
+const circle = new Circle("Red", 12);
 console.log(circle.calculateArea());
 
-class Rectangle implements IContentsOfFigures {
-  readonly color: string;
-  readonly name: string;
+// ---------------------------------------------------
+class Rectangle extends Figure {
   public width: number;
   public height: number;
-  constructor(name: string, color: string, width: number, height: number) {
-    this.name = name;
-    this.color = color;
+
+  constructor(color: string, width: number, height: number) {
+    super("Rectangle", color);
     this.width = width;
     this.height = height;
   }
@@ -39,21 +43,21 @@ class Rectangle implements IContentsOfFigures {
     const area = this.width * this.height;
     return area;
   }
+
   print(): string {
-    return "Area of rectangle = length * width";
+    return "Area of rectangle = width * height";
   }
 }
 
-const rectangle = new Rectangle("Rectangle", "Green", 22, 11);
+const rectangle = new Rectangle("Green", 22, 11);
 console.log(rectangle.calculateArea());
 
-class Square implements IContentsOfFigures {
-  readonly name: string;
-  readonly color: string;
+// ---------------------------------------------------
+class Square extends Figure {
   public side: number;
-  constructor(name: string, color: string, side: number) {
-    this.name = name;
-    this.color = color;
+
+  constructor(color: string, side: number) {
+    super("Square", color);
     this.side = side;
   }
 
@@ -61,31 +65,32 @@ class Square implements IContentsOfFigures {
     const squareArea = this.side * this.side;
     return squareArea;
   }
+
   print(): string {
-    return "Area of a square = Side * Side = S2";
+    return "Area of a square = Side * Side";
   }
 }
 
-const square = new Square("Square", "Yellow", 23);
+const square = new Square("Yellow", 23);
 console.log(square.calculateArea());
 console.log(square.print());
 
-class Triangle implements IContentsOfFigures {
-  readonly name: string;
-  readonly color: string;
+// ---------------------------------------------------
+class Triangle extends Figure {
   public base: number;
   public height: number;
-  constructor(name: string, color: string, base: number, height: number) {
-    this.name = name;
-    this.color = color;
+
+  constructor(color: string, base: number, height: number) {
+    super("Triangle", color);
     this.base = base;
     this.height = height;
   }
+
   calculateArea(): number {
-    const squareTriangle = 0.5 * this.base * this.height;
-    return squareTriangle;
+    const triangleArea = 0.5 * this.base * this.height;
+    return triangleArea;
   }
 }
 
-const triangle = new Triangle("Triangle", "Green", 23, 11);
+const triangle = new Triangle("Green", 23, 11);
 console.log(triangle.calculateArea());
